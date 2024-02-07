f8fd6b9a913f17c94ebac6c1b2b8f0e820af71ed
Isaac Benitez <ibsandoval@gmail.com>
2024-02-07 10:19:03 -0800

    test awx authentication endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const config = require('./config');
 const axios = require('axios')
 
@@ -7,11 +8,68 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 
+//let cachedToken = '';
+//const getToken = async () => {
+//  try {
+//      if (cachedToken) {
+//        return cachedToken;
+//      }
+//      const loginUrl = `${config.airwallex.clientApiHost}/api/v1/authentication/login`;
+//      const loginHeader = {
+//        'x-client-id': config.airwallex.clientId,
+//        'x-api-key': config.airwallex.apiKey,
+//        'Content-Type': 'application/json'
+//      };
+//      const loginRes = await axios.post(
+//        loginUrl,
+//        {},
+//        {
+//          headers: loginHeader,
+//        },
+//      );
+//      const token = loginRes.data.token;
+//      cachedToken = token;
+//      // Cache token for a while
+//      setTimeout(() => (cachedToken = ''), 20 * 60 * 1000);
+//      return token;
+//  } catch (error) {
+//        console.error("Error generating request options");
+//        throw error;
+//    }
+//};
+//
+//try {
+//    const token = getToken();
+//} catch (err) {
+//    // Handle api error here
+//    console.log(err);
+//}
+//
+const getToken = async () => {
+    try {
+        return axios
+            .request({
+                url: `${config.airwallex.clientApiHost}/api/v1/authentication/login`,
+                method: 'post',
+                headers: {
+                    'x-client-id': config.airwallex.clientId,
+                    'x-api-key': config.airwallex.apiKey,
+                }
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.get("/", (req, res) => {
+
+app.get("/", async (req, res) => {
+    //const token = await getToken();
+    //console.log(token.data.token);
+
     res.render("index");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
