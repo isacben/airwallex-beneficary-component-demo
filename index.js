0fcf6009f63c5cb172332e11c437f21a2b3d230f
Isaac Benitez <ibsandoval@gmail.com>
2024-02-08 16:19:31 -0800

    adding client

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const config = require('./config');
 const axios = require('axios')
-
+const Base64 = require("js-base64");
 const express = require("express");
+const sdk = require("@airwallex/payouts-web-sdk");
+
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.set("view engine", "ejs");
 
@@ -14,7 +16,6 @@ const getToken = async () => {
       if (cachedToken) {
         return cachedToken;
       }
-      console.log("Requesting authentication token...");
       const loginUrl = `${config.airwallex.clientApiHost}/api/v1/authentication/login`;
       const loginHeader = {
         'x-client-id': config.airwallex.clientId,
@@ -39,12 +40,92 @@ const getToken = async () => {
     }
 };
 
+// Generate code_verifier
+const dec2hex = (dec) => {
+  return ('0' + dec.toString(16)).slice(-2);
+};
+
+const generateCodeVerifier = () => {
+  // generate random length for code_verifier which should be between 43 and 128
+  const length = Math.random() * (129-43) + 43;   
+  const array = new Uint32Array(length/2);
+  crypto.getRandomValues(array);
+
+  return Array.from(array, dec2hex).join('');
+};
+
+const codeVerifier = generateCodeVerifier();
+
+// Generate code_challenge
+const sha256 = (plain) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return crypto.subtle.digest('SHA-256', data);
+};
+
+const base64urlencode = (hashed) => {
+  const bytes = new Uint8Array(hashed);
+  const base64encoded = Base64.fromUint8Array(bytes, true);
+  return base64encoded;
+};
+
+const generateCodeChallengeFromVerifier = async (codeVerifier) => {
+  const hashed = await sha256(codeVerifier);
+  const base64encoded = base64urlencode(hashed);
+  return base64encoded;
+};
+
+
+const authAccount = async (codeChallange, token) => {
+  try {
+      const url = `${config.airwallex.clientApiHost}/api/v1/authentication/authorize`;
+      const loginHeader = {
+        'x-client-id': config.airwallex.clientId,
+        'x-api-key': config.airwallex.apiKey,
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      };
+      const response = await axios.post(
+        url,
+        {
+            code_challenge: codeChallange,
+            scope: ["w:awx_action:onboarding"]
+        },
+        {
+          headers: loginHeader,
+        },
+      );
+      return response.data.authorization_code;
+  } catch (error) {
+        console.error("Error generating request options");
+        throw error;
+    }
+};
 
 app.get("/", async (req, res) => {
     const token = await getToken();
-    console.log(token);
+
+    const codeChallange = await generateCodeChallengeFromVerifier(codeVerifier);
+    const authorizationCode = await authAccount(codeChallange, token);
+    console.log("Code verifier:", codeVerifier);
+    console.log("Authorization Code:", authorizationCode);
 
     res.render("index");
+    try {
+    await sdk.init({
+      langKey: 'en',
+      env: 'demo',
+      authCode: authorizationCode, 
+      clientId: config.airwallex.clientId,
+      codeVerifier: codeVerifier,
+    });
+    }
+    catch(error) {
+        console.log(error);
+    }
+
+    const beneficiaryComponentlement = sdk.createElement('beneficiaryForm');
+    beneficiaryComponentlement.mount('#beneficiary-form-container');
 });
 
 app.listen(port, () => {
